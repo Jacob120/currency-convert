c6f86f819f915d494cf60eb84bf73c168063977b
Jakub Kozik <jakubkozik120@gmail.com>
2022-05-26 21:30:53 +0200

    Add test USD -> PLN in ResultBox.test.js

--- src/components/ResultBox/ResultBox.test.js
+++ src/components/ResultBox/ResultBox.test.js
@@ -5,28 +5,43 @@ import ResultBox from './ResultBox';
   describe('Component ResultBox', () => {
 
     const testCases = [
-      { amount: '100', from: 'PLN', to: 'USD' },
-      { amount: '20', from: 'USD', to: 'PLN' },
-      { amount: '200', from: 'PLN', to: 'USD' },
-      { amount: '345', from: 'USD', to: 'PLN' },
+      { amount: '100'},
+      { amount: '20'},
+      { amount: '200'},
+      { amount: '345'},
     ];
 
+    const formatterPLN = new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'PLN'
+    });
+
+    const formatterUSD = new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    });
+    
+
     for (const testObj of testCases) {
-      const amountValue = parseInt(testObj.amount)
 
-      const formatter = new Intl.NumberFormat('en-US', {
-        style: 'currency',
-        currency: 'PLN',
-      });
+      const formatFromPLN = formatterPLN.format(testObj.amount).replace(/\u00a0/g, ' ');
+      const formatToUSD = (formatterUSD.format(testObj.amount / 3.5)).replace(/\u00a0/g, ' ');
 
-      const amountFormat = formatter.format(amountValue);
-      const amountResult = (amountValue / 3.5).toFixed(2);
-      const outputResult = (amountFormat +  ' = $' + amountResult).replace(/\u00a0/g, ' ');
+      const formatFromUSD = formatterUSD.format(testObj.amount).replace(/\u00a0/g, ' ');
+      const formatToPLN = (formatterPLN.format(testObj.amount * 3.5)).replace(/\u00a0/g, ' ');
 
       it('should render proper info about conversion when PLN -> USD', () => {
-        render(<ResultBox from="PLN" to="USD" amount={amountValue} />);
+        render(<ResultBox from="PLN" to="USD" amount={parseInt(testObj.amount)} />);
         const output = screen.getByTestId('output');
-        expect(output).toHaveTextContent(outputResult);
+        expect(output).toHaveTextContent(`${formatFromPLN} = ${formatToUSD}`);
       });    
+
+   
+
+      it('should render proper info about conversion when USD -> PLN', () => {
+        render(<ResultBox from="USD" to="PLN" amount={parseInt(testObj.amount)} />);
+        const output = screen.getByTestId('output');
+        expect(output).toHaveTextContent(`${formatFromUSD} = ${formatToPLN}`);
+      });   
     }
   });
