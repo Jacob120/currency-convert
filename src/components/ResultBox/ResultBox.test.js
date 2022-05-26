b5371bed8439bc7a2eb93bef96b1055e8145605c
Jakub Kozik <jakubkozik120@gmail.com>
2022-05-26 19:20:39 +0200

    Fix ResultBox.test.js

--- src/components/ResultBox/ResultBox.test.js
+++ src/components/ResultBox/ResultBox.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import ResultBox from './ResultBox';
 
   describe('Component ResultBox', () => {
