d6292b684ddf4fba623d6042f1d77036de243014
Jakub Kozik <jakubkozik120@gmail.com>
2022-05-27 18:36:35 +0200

    Change testCases name in ResultBox.test.js

--- src/components/ResultBox/ResultBox.test.js
+++ src/components/ResultBox/ResultBox.test.js
@@ -3,14 +3,14 @@ import { render, screen, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
 describe('Component ResultBox', () => {
-  const testCasesPLNUSD = [
+  const testCasesPLNTOUSD = [
     { amount: 100, expected: 'PLN 100.00 = $28.57' },
     { amount: 20, expected: 'PLN 20.00 = $5.71' },
     { amount: 200, expected: 'PLN 200.00 = $57.14' },
     { amount: 345, expected: 'PLN 345.00 = $98.57' },
   ];
 
-  const testCasesUSDPLN = [
+  const testCasesUSDTOPLN = [
     { amount: 100, expected: '$100.00 = PLN 350.00' },
     { amount: 200, expected: '$200.00 = PLN 700.00' },
     { amount: 150, expected: '$150.00 = PLN 525.00' },
@@ -21,7 +21,7 @@ describe('Component ResultBox', () => {
     render(<ResultBox from='PLN' to='USD' amount={100} />);
   });
 
-  for (const testObj of testCasesPLNUSD) {
+  for (const testObj of testCasesPLNTOUSD) {
     it('should render proper info about conversion PLN -> USD', () => {
       render(<ResultBox from='PLN' to='USD' amount={testObj.amount} />);
       const output = screen.getByTestId('output');
@@ -30,7 +30,7 @@ describe('Component ResultBox', () => {
     cleanup();
   }
 
-  for (const testObj of testCasesUSDPLN) {
+  for (const testObj of testCasesUSDTOPLN) {
     it('should render proper info about conversion USD -> PLN', () => {
       render(<ResultBox from='USD' to='PLN' amount={testObj.amount} />);
       const output = screen.getByTestId('output');
